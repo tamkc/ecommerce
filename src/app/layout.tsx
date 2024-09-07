03d2fbce1f7485d4e8be5169d33a86f4316994b1
kc <77198654+tamkc@users.noreply.github.com>
2024-09-06 21:59:06 -0400

    feat: Add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
-
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +31,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <main>
+          {children}
+
+          <Footer />
+        </main>
       </body>
     </html>
   );
